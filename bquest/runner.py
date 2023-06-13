327a8b08120881a6e46ce906f1dff5e512a0abd3
Benjamin Gutzmann <benjamin.gutzmann@ottogroup.com>
2023-06-13 17:01:29 +0200

    Add docstrings and typehints and refactor small parts of code

--- bquest/runner.py
+++ bquest/runner.py
@@ -24,7 +24,7 @@ class BQConfigSubstitutor:
     def _map_source_table_ids_to_mock_table_ids(self, source_tables: List[BQTable]) -> Dict[str, str]:
         """Match source tables with their mocks."""
         result = {}
-        table_id_mapping = {t.original_table_id: t.test_table_id for t in source_tables}
+        table_id_mapping = {t.original_table_id: t.fq_test_table_id for t in source_tables}
 
         # e.g. { "feed": "bquest.example_id" }
         for table_key, table_id in self._config["source_tables"].items():
@@ -57,7 +57,7 @@ class BQConfigSubstitutor:
         config = copy.deepcopy(self._config)
         config["start_date"] = start_date
         config["end_date"] = end_date
-        config["feature_table_name"] = feature_table_name.test_table_id
+        config["feature_table_name"] = feature_table_name.fq_test_table_id
         config["source_tables"] = self._map_source_table_ids_to_mock_table_ids(test_tables)
         return config
 
@@ -94,7 +94,7 @@ class BQConfigRunner(BaseRunner):
         dataset: str = "bquest",
         clean_up: bool = True,
     ):
-        super(BQConfigRunner, self).__init__(bq_client, dataset)
+        super().__init__(bq_client, dataset)
         self._bq_executor_func = bq_executor_func
         self._clean_up = clean_up
 
@@ -114,6 +114,8 @@ class BQConfigRunner(BaseRunner):
             end_date: the end date (e.g. 20190308)
             source_table_definitions: custom table definitions that replace the source tables of the BQ configuration
             substitutor:  a substitutor for BQ configurations
+            result_table_definition: optional result table definition used for creating an empty result table
+            templating_vars: variables that are inserted into the given bq configuration
         Returns:
             the contents of the results table
         """
@@ -146,7 +148,7 @@ class BQConfigFileRunner:
         source_table_definitions: List[BQTableDefinition],
         path_to_config: str,
         result_table_definition: Optional[BQTableDefinition] = None,
-        allow_partial_table_substitutions: bool = False,
+        allow_partial_table_substitutions: Optional[bool] = False,
         templating_vars: Optional[Dict[str, str]] = None,
     ) -> pandas.DataFrame:
         """Runs a BQ configuration file"""
@@ -172,8 +174,15 @@ class SQLRunner(BaseRunner):
         self,
         bq_client: bq.Client,
         dataset: str = "bquest",
-        clean_up: bool = True,
+        clean_up: Optional[bool] = True,
     ):
+        """
+
+        Args:
+            bq_client: BigQuery client used for interaction with BigQuery
+            dataset: dataset which will be used for testing
+            clean_up:  boolean if tables should be cleaned up
+        """
         super(SQLRunner, self).__init__(bq_client, dataset)
         self._bq_client = bq_client
         self._clean_up = clean_up
@@ -186,8 +195,18 @@ class SQLRunner(BaseRunner):
         string_replacements: Optional[Dict[str, str]] = None,
         result_table_definition: Optional[BQTableDefinition] = None,
     ) -> pandas.DataFrame:
-        # pylint: disable=unused-variable, missing-function-docstring
+        """
 
+        Args:
+            sql: SQL query that is being executed in BigQuery
+            source_table_definitions: source table definitions, list of BQTableDefinition
+            substitutions: substitutions for the given query
+            string_replacements: entire string replacements for the query, substitutions are placed before
+            result_table_definition: result table definition
+
+        Returns:
+            pandas DataFrame of result table
+        """
         if substitutions is None:
             substitutions = {}
 
@@ -216,6 +235,12 @@ class SQLFileRunner:
     """Class for running SQLFiles."""
 
     def __init__(self, sql_runner: SQLRunner, base_path: str) -> None:
+        """
+
+        Args:
+            sql_runner: SQLRunner used to execute SQL config
+            base_path: base path where SQL config is looked for
+        """
         self._sql_runner = sql_runner
         self._base_path = base_path
 
@@ -226,8 +251,17 @@ class SQLFileRunner:
         substitutions: Optional[Dict[str, str]] = None,
         string_replacements: Optional[Dict[str, str]] = None,
     ) -> pandas.DataFrame:
-        # pylint: disable=missing-function-docstring
+        """
 
+        Args:
+            path_to_sql: relative path to SQL config file based on base path
+            source_table_definitions: source table definitions
+            substitutions: substitutions for SQL query
+            string_replacements: entire string replacements for the query, substitutions are placed before
+
+        Returns:
+
+        """
         if substitutions is None:
             substitutions = {}
 
